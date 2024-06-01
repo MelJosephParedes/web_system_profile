2486b8733179e9e485db7eb39e93f3c30c06802a
MelJosephParedes <paredesmeljoseph@gmail.com>
2024-06-01 15:52:47 +0800

    second commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import Navbar from '../components/Navbar';
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <h1>Home Page</h1>
       <p>Welcome to the home page!</p>
     </div>
